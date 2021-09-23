1ec86cd88127a8146e8098038802e99873c6edff
raysimanta <simantaray89@gmail.com>
2021-09-23 21:46:45 +0530

    footer2

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import styled from 'styled-components';
-
+import React from "react";
+import styled from "styled-components";
 
 function Footer() {
-    return (
-        <Container>
-            <h1>Footer</h1>
-        </Container>
-    )
+  return (
+    <Container>
+      <h1>Copyright 2019</h1>
+    </Container>
+  );
 }
 
-export default Footer
+export default Footer;
 
-const Container=styled.div``;
+const Container = styled.div`
+  margin: 8px -8px 0 -8px;
+  padding: 1px 50px;
+  background-color: rgba(0, 41, 63, 0.9);
+  color: white;
+  h1{
+      font-size:12px ;
+  }
+`;
